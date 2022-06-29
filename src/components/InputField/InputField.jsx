4a0a404475abeec96c8971288d976d468903fdec
GamaRayL <gamaizingg@gmail.com>
2022-06-29 13:33:34 +0300

    Button has been added, which adds tasks

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,14 +1,19 @@
+import { AddBoxOutlined } from "@mui/icons-material";
 import css from "./styles.module.css";
 
 export const InputField = ({ addTask, setText, text }) => {
   return (
-    <form onSubmit={(e) => addTask(e)}>
+    <form className={css.form} onSubmit={(e) => addTask(e)}>
       <input
         value={text}
         onChange={(e) => setText(e.target.value)}
         type="text"
         placeholder="What need to be done?"
         className={css.textField}
+      />
+      <AddBoxOutlined
+        onClick={(e) => addTask(e)}
+        className={css.addIcon}
       />
     </form>
   );
