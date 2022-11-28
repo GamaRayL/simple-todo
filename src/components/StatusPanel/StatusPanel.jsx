e0698644317704a209095e2a96f7d2b5817909b0
GamaRayL <gamaizingg@gmail.com>
2022-11-28 16:09:41 +0300

    refactor: StatusPanel destructurization

--- src/components/StatusPanel/StatusPanel.jsx
+++ src/components/StatusPanel/StatusPanel.jsx
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { changeFilter } from "store/filterSlice";
 import { selectAllTodos } from "store/selectors";
 import { removeCompletedTodos } from "store/todoSlice";
+import { FilterElement } from "./FilterElement";
 import css from "./styles.module.css";
 
 export const StatusPanel = () => {
@@ -9,41 +10,17 @@ export const StatusPanel = () => {
   const todos = useSelector(selectAllTodos);
   const itemsLeft = todos.filter((todo) => todo.isCompleted !== true).length;
 
+  const onChangeHandler = (e) => {
+    dispatch(changeFilter(e.target.id));
+  };
+
   return (
     <div className={css.panel}>
       <div className={css.todoLeft}>{`${itemsLeft} ${"items left"}`}</div>
       <div className={css.filters}>
-        <input
-          className={css.radio}
-          onChange={() => dispatch(changeFilter("all"))}
-          type="radio"
-          defaultChecked
-          name="filter"
-          id="all"
-        />
-        <label htmlFor="all" className={css.filterItem}>
-          All
-        </label>
-        <input
-          className={css.radio}
-          onChange={() => dispatch(changeFilter("active"))}
-          type="radio"
-          name="filter"
-          id="active"
-        />
-        <label htmlFor="active" className={css.filterItem}>
-          Active
-        </label>
-        <input
-          className={css.radio}
-          onChange={() => dispatch(changeFilter("completed"))}
-          type="radio"
-          name="filter"
-          id="completed"
-        />
-        <label htmlFor="completed" className={css.filterItem}>
-          Completed
-        </label>
+        <FilterElement onChange={onChangeHandler} checked id="all" />
+        <FilterElement onChange={onChangeHandler} id="active" />
+        <FilterElement onChange={onChangeHandler} id="completed" />
       </div>
       <div
         className={css.clearCompletedTodo}
